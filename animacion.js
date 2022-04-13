40ebd5e006196742d605044baeeb393505035afb
Axel <AxelRoldan@outlook.es>
2022-04-12 21:16:22 -0300

    Se agrego presentacion
    Se agrego texto historia de vida

--- animacion.js
+++ animacion.js
@@ -1,89 +1,10 @@
-let valor = document.getElementById("efecto")
-let valor1 = document.getElementById("efecto1")
-let valor2 = document.getElementById("efecto2")
-let valor3 = document.getElementById("efecto3")
-var contador = 22;
-let boolean = true
+document.querySelector(".boton").addEventListener("click", e => {
 
-valor.addEventListener("click", e => {
+    console.log("evento realizado")
 
-    if(boolean){
-        document.querySelectorAll(".circulo").forEach( e => {
-        
-            e.style.setProperty("top",`${contador}%`)
-            contador += 22
-        })
-        boolean = false
-    }
-    else{
-        document.querySelectorAll(".circulo").forEach( e => {
-        
-            e.style.setProperty("top","0")
-            contador = 22
-        })
-        boolean = true
-    }
-
-})
-
-valor1.addEventListener("click", e => {
-
-    if(boolean){
-        document.querySelectorAll(".circulo1").forEach( e => {
-        
-            e.style.setProperty("top",`${contador}%`)
-            contador += 22
-        })
-        boolean = false
-    }
-    else{
-        document.querySelectorAll(".circulo1").forEach( e => {
-        
-            e.style.setProperty("top","0")
-            contador = 22
-        })
-        boolean = true
-    }
-
-})
-
-valor2.addEventListener("click", e => {
-
-    if(boolean){
-        document.querySelectorAll(".circulo2").forEach( e => {
-        
-            e.style.setProperty("top",`${contador}%`)
-            contador += 22
-        })
-        boolean = false
-    }
-    else{
-        document.querySelectorAll(".circulo2").forEach( e => {
-        
-            e.style.setProperty("top","0")
-            contador = 22
-        })
-        boolean = true
-    }
-
-})
-
-valor3.addEventListener("click", e => {
-
-    if(boolean){
-        document.querySelectorAll(".circulo3").forEach( e => {
-        
-            e.style.setProperty("top",`${contador}%`)
-            contador += 22
-        })
-        boolean = false
-    }
-    else{
-        document.querySelectorAll(".circulo3").forEach( e => {
-        
-            e.style.setProperty("top","0")
-            contador = 22
-        })
-        boolean = true
-    }
+    document.querySelector(".agradecimientos").classList.add("efecto")
+    document.querySelector(".caja").classList.add("efecto1")
+    document.querySelector(".boton").classList.add("efecto1")
+    document.querySelector(".logo").classList.add("efecto1")
+    document.querySelector(".logo1").classList.add("efecto1")
 })
